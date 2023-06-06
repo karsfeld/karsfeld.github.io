4033b7efee60a681984890681c650af99c3a3b32
maalrocks <spammersohn@gmail.com>
2023-06-06 12:33:27 +0200

    changed index

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Meine Persönliche Webseite';
-export const SITE_DESCRIPTION = 'Wilkommen auf meiner Webseite';
+export const SITE_TITLE = '- Karsfeld - ';
+export const SITE_DESCRIPTION = 'Wilkommen';
